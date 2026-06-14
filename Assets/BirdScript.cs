89ddbe4ee6fde493b1c459fb6b86dfd1383ec457
Tysi3k22 <kacpert.kasiak@gmail.com>
2026-06-14 07:01:59 +0200

    added birds movement

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int flopStrenght = 6;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        flop();
+    }
+
+    void flop()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            rb.linearVelocity = Vector2.up * (flopStrenght * Time.deltaTime);
+        }
+    }
+}
